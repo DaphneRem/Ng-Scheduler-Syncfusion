258f1677dbc41fe82b0dc55433a565ff68caf566
Daphne <remalidaphne@gmail.com>
2018-11-16 19:14:48 +0100

    add syncfusion services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,81 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
+import { enableRipple } from '@syncfusion/ej2-base';
+
+import { extend } from '@syncfusion/ej2-base';
+
+import {
+    EventSettingsModel,
+    GroupModel
+    // DayService,
+    // WeekService,
+    // WorkWeekService,
+    // MonthService,
+    // AgendaService,
+    // MonthAgendaService,
+    // TimelineViewsService,
+    // TimelineMonthService,
+    // DragAndDropService
+  } from '@syncfusion/ej2-angular-schedule';
+
+// import { defaultData } from './datasource';
+import { timelineResourceData, resourceData } from './datasource';
+
+
+// enable ripple effects
+enableRipple(true);
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styles: [`
+    .timeline-resource-grouping.e-schedule:not(.e-device) .e-agenda-view .e-content-wrap table td:first-child {
+        width: 90px;
+    }
+    .timeline-resource-grouping.e-schedule .e-agenda-view .e-resource-column {
+        width: 100px;
+    }
+  `],
+    encapsulation: ViewEncapsulation.None,
+  // styleUrls: ['./app.component.scss'],
+  // providers: [
+  //   DayService,
+  //   WeekService,
+  //   WorkWeekService,
+  //   MonthService,
+  //   AgendaService
+  // ]
 })
 export class AppComponent {
-  title = 'app';
+  // public selectedDate: Date = new Date(2018, 1, 15);
+  // public showWeekend: Boolean = false;
+  // public eventSettings: EventSettingsModel = { dataSource: defaultData };
+
+    public selectedDate: Date = new Date(2018, 3, 4);
+    public group: GroupModel = {
+        byGroupID: false,
+        resources: ['Categories', 'Users']
+        // resources: ['Categories']
+        // resources: ['Projects', 'Categories']
+    };
+    // public projectDataSource: Object[] = [
+    //     { text: 'PROJECT 1', id: 1, color: '#cb6bb2' },
+    //     { text: 'PROJECT 2', id: 2, color: '#56ca85' },
+    //     { text: 'PROJECT 3', id: 3, color: '#df5286' }
+    // ];
+    public categoryDataSource: Object[] = [
+        { text: 'Régie A', id: 1, color: '#008eaa' },
+        { text: 'Régie B', id: 2, color: '#d20962' },
+        { text: 'Régie C', id: 3, color: '#ffc845' },
+        { text: 'Régie D', id: 4, color: '#7ac143' }
+    ];
+    public userDataSource: Object[] = [
+        { text: 'User1', id: 1},
+        { text: 'User2', id: 2},
+        { text: 'User3', id: 3},
+        { text: 'User4', id: 4}
+    ];
+    public allowMultiple: Boolean = true;
+    public eventSettings: EventSettingsModel = {
+        dataSource: <Object[]>extend([], resourceData.concat(timelineResourceData), null, true)
+    };
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,52 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 
+// Imported syncfusion button module from buttons package
+import { ButtonModule } from '@syncfusion/ej2-angular-buttons';
+
+// import the ScheduleModule for the Schedule component
+import { ScheduleModule } from '@syncfusion/ej2-angular-schedule';
+
+import {
+  DayService,
+  WeekService,
+  WorkWeekService,
+  MonthService,
+  AgendaService,
+  MonthAgendaService,
+  DragAndDropService,
+  TimelineViewsService,
+  TimelineMonthService,
+  ResizeService
+} from '@syncfusion/ej2-angular-schedule';
+
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    // Registering EJ2 Button Module
+    ButtonModule,
+    ScheduleModule
   ],
-  providers: [],
+  providers: [
+    DayService,
+    WeekService,
+    WorkWeekService,
+    MonthService,
+    AgendaService,
+    MonthAgendaService,
+    DragAndDropService,
+    TimelineViewsService,
+    TimelineMonthService,
+    ResizeService
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
